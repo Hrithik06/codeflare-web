d7fe5a884f2a70ab564b9a54e66a34801a086523
Hrithik06 <el99el03@gmail.com>
2025-12-25 18:26:15 +0530

    Removed BASE_URL taking value from env variable as it was already being satisfied by nginx proxy in prod and for local dev there is vite proxy to replace /api with backend.

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), visualizer({
-    filename: "bundle-stats.html", // Puts the graph file at project root
-    open: true,                    // Auto-opens after build
-    template: "treemap",            // Use treemap view (better visual)
-    gzipSize: true,
-    brotliSize: true,
-  })]
+	plugins: [
+		react(),
+		tailwindcss(),
+		visualizer({
+			filename: "bundle-stats.html", // Puts the graph file at project root
+			open: true, // Auto-opens after build
+			template: "treemap", // Use treemap view (better visual)
+			gzipSize: true,
+			brotliSize: true,
+		}),
+	],
+	// });
 
-})
+	//NOTE:Here vite is proxying in prod nginx does the proxying
+
+	// export default defineConfig({
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://localhost:7777",
+				changeOrigin: true,
+				secure: false,
+				rewrite: (path) => path.replace(/^\/api/, ""),
+			},
+		},
+	},
+});
