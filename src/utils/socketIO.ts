41608eb781267e3807e494095c02acf5d527f1f1
Hrithik06 <el99el03@gmail.com>
2026-05-29 18:28:40 +0530

    refactor: update socket connection logic to use environment variable for API URL

--- src/utils/socketIO.ts
+++ src/utils/socketIO.ts
@@ -1,15 +1,20 @@
 import { io } from "socket.io-client";
-export const createSocketConnection = () => {
-	return io("/", {
-		path: "/api/socket.io",
-	});
+// export const createSocketConnection = () => {
+//   return io("/", {
+//     path: "/api/socket.io",
+//   });
 
-	//NOTE: I dont need this as I have vite proxy
-	// if (location.hostname === "localhost") {
-	// 	return io("http://localhost:7777");
-	// } else {
-	// 	return io("/", {
-	// 		path: "/api/socket.io",
-	// 	});
-	// }
+//   //NOTE: I dont need this as I have vite proxy
+//   // if (location.hostname === "localhost") {
+//   // 	return io("http://localhost:7777");
+//   // } else {
+//   // 	return io("/", {
+//   // 		path: "/api/socket.io",
+//   // 	});
+//   // }
+// };
+export const createSocketConnection = () => {
+  return io(import.meta.env.VITE_API_URL, {
+    withCredentials: true,
+  });
 };
