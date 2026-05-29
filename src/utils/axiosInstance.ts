adc470101c32b03691ae2e153780baeb19b1d5df
Hrithik06 <el99el03@gmail.com>
2026-05-29 13:14:12 +0530

    fix: update axios instance configuration and formatting

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -2,15 +2,17 @@ import axios from "axios";
 // import { BASE_URL } from "./constants";
 
 const api = axios.create({
-	// baseURL: BASE_URL,
-	baseURL: "/api",
+  // baseURL: BASE_URL,
+  // baseURL: "/api",
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
 });
 
 api.interceptors.response.use(
-	(response) => response,
-	(error) => {
-		//TODO: whatever you want to do with the error
-		throw error;
-	},
+  (response) => response,
+  (error) => {
+    //TODO: whatever you want to do with the error
+    throw error;
+  },
 );
 export default api;
